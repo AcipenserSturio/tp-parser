c8ad2040a7c0cc772bdd19bd6d50f65762ce0dfb
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2024-04-20 21:51:20 +0500

    Improve testing and tokenisation

--- src/tp_parser/__main__.py
+++ src/tp_parser/__main__.py
@@ -1,15 +1,42 @@
 import argparse
 import re
+import csv
 
 import nltk
+import pandas as pd
 
 SOURCE = "assets/my_grammar.txt"
 SENTENCES = "assets/test_sentences.txt"
+TATOEBA = "assets/tok_sentences.tsv"
+
+# https://stackoverflow.com/questions/25735644/python-regex-for-splitting-text-into-sentences-sentence-tokenizing#25736082
+# Modified to include "!"
+SENT_BOUNDARY = r"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|\!|\:)\s"
+VOCATIVE_BOUNDARY = r", (?=.* o[\.\?\!]?$)"
 
 with open(SOURCE) as f:
     grammar = f.read()
 grammar1 = nltk.CFG.fromstring(grammar)
 rd_parser = nltk.RecursiveDescentParser(grammar1)
+
+
+def get_test_sentences():
+    with open(TATOEBA) as f:
+        for _, _, text in csv.reader(f, delimiter="\t"):
+            text = re.sub(VOCATIVE_BOUNDARY, ". ", text)
+            sents = re.split(SENT_BOUNDARY, text)
+            for sent in sents:
+                yield sent
+
+    with open(SENTENCES) as f:
+        for line in f.readlines():
+            line = line.strip()
+            if not line:
+                continue
+            if "#" in line:
+                continue
+            yield line
+
 
 def tokenize(sent: str) -> list:
     sent = re.sub(r"[^\w\s]", "", sent)
@@ -26,23 +53,32 @@ def tokenize(sent: str) -> list:
     sent = [word for word in sent if word != "_"]
     return sent
 
+
 def parse(sent: str):
     sent = tokenize(sent)
     trees = rd_parser.parse(sent)
     try:
         tree = next(trees)
     except StopIteration:
-        raise ValueError(f"No parse for {sent}")
-    tree.pretty_print()
+        # raise ValueError(f"No parse for {sent}")
+        return None
+    return tree
+
 
 def test_parser():
-    with open(SENTENCES) as f:
-        sentences = [line.strip() for line in f.readlines() if line.strip() and "#" not in line]
-        # print(sentences)
+    cases = []
+    for index, sent in enumerate(get_test_sentences()):
+        tree = parse(sent)
+        # tree.pretty_print()
 
-    for sent in sentences:
-        print("=============================================")
-        parse(sent)
+        tree = tree.pformat(margin=99999) if tree else "FAILED"
+        cases.append([sent, tree])
+        if not index % 10:
+            print(index)
+        if index == 100:
+            break
+    pd.DataFrame(cases, columns=["Sentence", "Parse"]).to_csv("test.csv")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
